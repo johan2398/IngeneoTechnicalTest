420cfd85643cb5e81c01b882cdd8723ba57d174d
Johan Casagua <joseca2398@hotmail.com>
2023-10-23 23:40:33 -0500

    Create the project

---
+++ ingeneo_technical_test/ingeneo_technical_test/src/main/java/com/example/ingeneo_technical_test/IngeneoTechnicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ingeneo_technical_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngeneoTechnicalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngeneoTechnicalTestApplication.class, args);
+	}
+
+}
---
+++ ingeneo_technical_test/ingeneo_technical_test/src/test/java/com/example/ingeneo_technical_test/IngeneoTechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ingeneo_technical_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngeneoTechnicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
